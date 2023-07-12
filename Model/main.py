bfc5b929ec277b3fa959a9f20b6c7e9b63fd3fc6
Tharuka_00 <tharusenevi2000@gmail.com>
2023-07-12 09:45:04 +0530

    updated empty values with means and drew histograms to the updated dataset

--- Model/main.py
+++ Model/main.py
@@ -4,10 +4,17 @@ import matplotlib.pyplot as plt
 import seaborn as sb
 
 df = pd.read_csv('D:\Projects\ML\Wine Quality Prediction ML model\winequalityN.csv')
-print(df.head(2))
+#print(df.head(2))
 
-#types of data present in each column of the dataset
-print(df.info())
+# Let’s inpute the missing values by means as the data present in the different columns are continuous values.
+for col in df.columns:
+    if df[col].isnull().sum() > 0:
+        # fillna() is a method in pandas to fill the missing values with a specific value (here it is mean)
+        df[col] = df[col].fillna(df[col].mean())
 
-#descriptive statistical measures of the dataset.
-print(df.describe().T)
+# overall total sum of empty columns
+df.isnull().sum().sum()
+
+# Let’s draw the histogram to visualise the distribution of the data with continuous values in the columns of the dataset.
+df.hist(bins=20, figsize=(10, 10))
+plt.show()
